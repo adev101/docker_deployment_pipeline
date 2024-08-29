2ac4f5d24fbb9c99182e5b4398ae9f28e2e54629
abhinav dev <39181811+adev101@users.noreply.github.com>
2024-08-29 21:27:30 +0530

    Update containerCheck.py

--- containerCheck.py
+++ containerCheck.py
@@ -1,36 +1,38 @@
+
 import os
 import subprocess
 
 
-imageList = subprocess.Popen("docker images", shell=True, stdout=subprocess.PIPE)
+# imageList = subprocess.Popen("docker images", shell=True, stdout=subprocess.PIPE)
 
-print(imageList.stdout)
+# print(imageList.stdout)
 verison=""
 
-imagename = "abhi2086/test"
+imageName = "abhi2086/test"
 
 containerList = subprocess.Popen("docker ps", shell=True, stdout=subprocess.PIPE)
 
-for line in imageList.stdout:
-    line = line.decode('utf-8')
+# for line in imageList.stdout:
+#     line = line.decode('utf-8')
 
-    tempArr=line.split(" ")
+#     tempArr=line.split(" ")
 
 
-    if imagename in line:
-        imageId = tempArr[31]
+#     if imagename in line:
+#         imageId = tempArr[31]
 
-        print("Latest Image Id: " + imageId)
+#         print("Latest Image Id: " + imageId)
 
-        break
+#         break
 
 
 
 for line in containerList.stdout:
     line = line.decode('utf-8')
 
-    if imageId in line:
+    if imageName in line:
         print("Container found running. " )
 
         tempArr=line.split(" ")
         os.system("docker kill "+ tempArr[0])
+        print("Container killed - "+ tempArr[0])
